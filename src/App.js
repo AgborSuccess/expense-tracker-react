7079012c5ae129713662599e75fd1a3993d31de8
AgborSuccess <agborsuccess2007@gmail.com>
2023-08-21 00:23:18 -0700

    commit message: Completed the Expense Tracker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Header } from './components/Header'
+import { Balance } from './components/Balance'
+import { IncomeExpense } from './components/IncomeExpense'
+import { TransactionList } from './components/TransactionList'
+import { AddTransaction } from './components/AddTransaction'
+
+import { GlobalProvider } from './context/GlobalState'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+     <Header/>
+     <div className="container">
+        <Balance/>
+     </div>
+     <IncomeExpense/>
+     <TransactionList/>
+     <AddTransaction/>
+
+    </GlobalProvider>  
   );
 }
 
